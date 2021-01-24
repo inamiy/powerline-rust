05f9bc49810f3b537f12b1aa2a587414981b4055
Yasuhiro Inami <inamiy@gmail.com>
2021-01-24 15:45:16 +0900

    inamiy's tweak

--- src/bin/powerline.rs
+++ src/bin/powerline.rs
@@ -8,17 +8,17 @@ use powerline::modules::Time;
 fn main() -> powerline::R<()> {
 	let mut prompt = powerline::Powerline::new();
 
-	#[cfg(feature = "time")]
-	prompt.add_module(Time::<SimpleTheme>::with_time_format("%H:%M:%S"))?;
+	// #[cfg(feature = "time")]
+	// prompt.add_module(Time::<SimpleTheme>::with_time_format("%H:%M:%S"))?;
 
-	prompt.add_module(User::<SimpleTheme>::new())?;
-	prompt.add_module(Host::<SimpleTheme>::new())?;
+	// prompt.add_module(User::<SimpleTheme>::new())?;
+	// prompt.add_module(Host::<SimpleTheme>::new())?;
 	prompt.add_module(Cwd::<SimpleTheme>::new(45, 4, false))?;
 	prompt.add_module(Git::<SimpleTheme>::new())?;
 	prompt.add_module(ReadOnly::<SimpleTheme>::new())?;
-	prompt.add_module(Cmd::<SimpleTheme>::new())?;
+	// prompt.add_module(Cmd::<SimpleTheme>::new())?;
 	// prompt.add_module(VirtualEnv::<SimpleTheme>::new())?;
-	// prompt.add_module(ExitCode::<SimpleTheme>::new())?;
+	prompt.add_module(ExitCode::<SimpleTheme>::new())?;
 
 	println!("{}", prompt);
 	Ok(())
